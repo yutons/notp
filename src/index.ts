b44a04146334fab94779a51f60b1ece665128ab7
雨桐 <yuton.yao@qq.com>
2025-08-13 21:26:41 +0800

    feat: 创建入口文件并导出核心模块

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {Algorithm, Default} from './core/default'
+import {HOTP} from './core/hotp';
+import {TOTP} from './core/totp';
+
+const notp = {
+    Algorithm,
+    Default,
+    HOTP,
+    TOTP
+};
+
+export default notp;
